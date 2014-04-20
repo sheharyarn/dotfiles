680ae097977426f8cc04ff768de860f1c35e84fe
Sheharyar Naseer <sheharyar92@live.com>
2014-04-20 21:11:37 +0500

    Added more git aliases

--- Bash/_bash_aliases.sh
+++ Bash/_bash_aliases.sh
@@ -1,15 +1,23 @@
 # ALIASES
 # #######
 
+# List
 alias l='ls -FG'
 alias la='ls -FGal'
 
+# CD
 alias ~='cd ~'
 alias ..='cd ..'
 alias ....='cd ../../'
 
+# Git
 alias gs='git status'
+alias ga='git add -A'
+alias gc='git commit -m'
+alias gp='git push'
 alias gl='git log --oneline'
 alias gla='git log'
 
+# Sublime
 alias subl='sublime'
+alias edit='sublime'
