ae065b685045cc8441c5292e11d01ef198cafca8
Sheharyar Naseer <sheharyar92@live.com>
2014-05-01 18:26:49 +0500

    More Git & RoR Aliases

--- Bash/_bash_aliases.sh
+++ Bash/_bash_aliases.sh
@@ -17,8 +17,14 @@ alias ga='git add -A'
 alias gc='git commit -m'
 alias gp='git push'
 alias gl='git log --oneline'
+alias gac='git add -A && git commit -m'
 alias gla='git log'
+alias gpull='git pull origin'
 
 # Sublime
 alias subl='sublime'
 alias edit='sublime'
+
+# Ruby / Rails
+alias rs='rails s'
+alias rc='rails c'
