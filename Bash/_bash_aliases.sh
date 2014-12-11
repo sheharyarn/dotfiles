582205c0d29e0ef34aa6fec8c4cde937b1bd396d
Sheharyar Naseer <sheharyar92@live.com>
2014-12-12 01:33:03 +0500

    Colorful git log

--- Bash/_bash_aliases.sh
+++ Bash/_bash_aliases.sh
@@ -16,7 +16,8 @@ alias gs='git status'
 alias ga='git add -A'
 alias gc='git commit -m'
 alias gp='git push'
-alias gl='git log --oneline'
+#alias gl='git log --oneline'
+alias gl="git log --graph --pretty=format:'%C(red)%h - %Creset%s'"  ## https://coderwall.com/p/euwpig/a-better-git-log
 alias gac='git add -A && git commit -m'
 alias gla='git log'
 alias gpull='git pull origin'
@@ -24,7 +25,7 @@ alias gitcount='git rev-list HEAD --count'
 alias gpdeploy='gp && cap production deploy'
 
 # iA Writer
-alias md='open -a "iA Writer"'
+alias md='open -a "iA Writer Pro"'
 
 # Sublime
 alias subl='sublime'
@@ -33,6 +34,7 @@ alias edit='sublime'
 # Ruby / Rails
 alias rs='rails s'
 alias rc='rails c'
+alias rg='rails g'
 
 # Random
 alias refresh="source ~/.bash_profile"
