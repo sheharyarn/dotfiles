cb4162916cf598f5abe6d597488e27418c8e5d3b
Sheharyar Naseer <sheharyar92@live.com>
2014-12-14 06:26:30 +0500

    OSX Defaults

---
+++ OSX/defaults.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# OSX Defaults
+# ============
+
+# Hide Icons on Desktop
+defaults write com.apple.finder CreateDesktop -bool false && killall Finder
+
+# Hide Safari's bookmark bar.
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
