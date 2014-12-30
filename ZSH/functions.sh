84ab65ab3537e8f074d5d1d8a60a463d9c8d3915
Sheharyar Naseer <sheharyar92@live.com>
2014-12-31 00:18:14 +0500

    WordFrequency function and rvm path for bins

--- ZSH/functions.sh
+++ ZSH/functions.sh
@@ -33,3 +33,21 @@ extract () {
     echo "'$1' is not a valid file"
   fi
 }
+
+# Get Word Freqeuncy of Files
+function wordfrequency() {
+  awk '
+     BEGIN { FS="[^a-zA-Z]+" } {
+         for (i=1; i<=NF; i++) {
+             word = tolower($i)
+             words[word]++
+         }
+     }
+     END {
+         for (w in words)
+              printf("%3d %s\n", words[w], w)
+     } ' | sort -rn
+}
+
+
+
