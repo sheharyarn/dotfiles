9d85a293923fa0e3cfed1fe70e0db236c7a28b99
Sheharyar Naseer <sheharyar92@live.com>
2015-01-08 17:34:51 +0500

    More awesome git aliases

--- Git/aliases.sh
+++ Git/aliases.sh
@@ -5,12 +5,19 @@
 
 # Status and Information
 alias        gs='git status'
+alias       gss='git status -s'
+
+alias        gd='git diff'
+alias       gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+
 alias        gl='git log --decorate --color         --oneline'
 alias       glg='git log --decorate --color --graph --oneline'
 alias       gla='git log --decorate --color --graph'
-alias      glaa='git log --decorate --color --graph            --stat --all'
+alias      glaa='git log --decorate --color --graph           --stat --all'
 
-  # gl="git log --graph --pretty=format:'%C(red)%h - %Creset%s'"  ## https://coderwall.com/p/euwpig/a-better-git-log
+  # gl="git log --graph --pretty=format:'%C(red)%h - %Creset%s'"  
+  ## https://coderwall.com/p/euwpig/a-better-git-log
+
 
 # Staging and Commiting
 alias        gc='git commit -m'
@@ -18,13 +25,18 @@ alias        ga='git add -A'
 alias       gac='git add -A && git commit -m'
 alias       guc='git add -u && git commit -m'
 
+
 # Pushing, Pulling and Deploying
 alias        gp='git push'
 alias     gpull='git pull origin'
-alias     gpdep='gp && cap production deploy'
+alias     gpdep='gpdeploy'
 alias  gpdeploy='gp && cap production deploy'
 
+
 # Others
+alias       grt='gitroot'
+alias   gitroot='cd $(git rev-parse --show-toplevel || echo ".")'
 alias  gitcount='git rev-list HEAD --count'
+alias   gsearch='gitsearch'
 alias gitsearch='gla -p -S'
 
