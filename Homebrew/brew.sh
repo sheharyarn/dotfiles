af993b2ffbf63e4f593aa8550ed09270ce9c2a67
Sheharyar Naseer <sheharyar92@live.com>
2015-01-18 05:35:07 +0500

    Rants

--- Homebrew/brew.sh
+++ Homebrew/brew.sh
@@ -20,11 +20,11 @@ brew tap homebrew/dupes
 # Install binaries
 # ----------------
 
-brew install git                            # see dotfiles/git
+brew install git                            # : dotfiles/git
 brew install wget
 brew install node
-brew install nano                           # see dotfiles/nano
-brew install vim
+brew install nano                           # : dotfiles/nano
+brew install vim                            # : dotfiles/vim
 brew install heroku-toolbelt
 brew install brew-cask
 brew install tor
@@ -39,22 +39,22 @@ brew install torsocks
 brew cask install firefox
 brew cask install google-chrome
 brew cask install vlc
-brew cask install mpv                       # see dotfiles/osx/mpv
+brew cask install mpv                       # : dotfiles/osx/mpv
 brew cask install utorrent
 brew cask install skype
 brew cask install jumpshare                 # fuck dropbox
 brew cask install android-file-transfer
 
 #> Dev Apps
-brew cask install sublime-text              # see dotfiles/sublime/2
+brew cask install sublime-text              # : dotfiles/sublime/2
 brew cask install atom
-brew cask install android-studio
+brew cask install android-studio            # point it to your huge sdk folder
 brew cask install testflight
 
 #> Not that important
 brew cask install teamviewer
 brew cask install lastfm
-brew cask install couleurs
+brew cask install couleurs                  # i'm conflicted, i love this app
 brew cask install chromecast
 brew cask install ps3-media-server
 brew cask install tunnelbear
