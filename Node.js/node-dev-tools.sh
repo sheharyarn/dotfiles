4783133f069089ef8362a45ec21ba77cce8f0a7c
Sheharyar Naseer <sheharyar92@live.com>
2015-05-22 17:12:26 +0500

    Node Dev Tool script

---
+++ Node.js/node-dev-tools.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Node Dev Tools
+# --------------
+# (Make sure node.js is installed via brew)
+
+npm install -g express
+npm install -g nodemon
+npm install -g node-dev
+
+npm install -g grunt-cli
+npm install -g gulp
+npm install -g bower
+npm install -g webpack
+
