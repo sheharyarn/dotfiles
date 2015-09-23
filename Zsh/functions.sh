8339b351580ee820b63e7a68a2b2a45786c1c0ec
Sheharyar Naseer <hello@sheharyar.me>
2015-09-23 18:00:58 +0500

    updated transfer function

--- Zsh/functions.sh
+++ Zsh/functions.sh
@@ -89,7 +89,7 @@ transfer() {
     # check arguments
     if [ $# -eq 0 ]; 
     then 
-        echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
+        echo "No arguments specified. Usage:\ntransfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
         return 1
     fi
 
