39a230aace58568bd6f5998733388430e5331b2e
Sheharyar Naseer <hello@sheharyar.me>
2016-09-07 01:42:44 +0500

    myip and pubip commands

--- Zsh/aliases.sh
+++ Zsh/aliases.sh
@@ -16,9 +16,11 @@ alias     crap="cd ~/crap"
 # Random
 alias zshrc="$EDITOR $zshrc"
 alias reload!="source $zshrc"
-alias myip='ifconfig en0 inet | grep inet'
 alias fsize='du -hs'
 alias dsize='du -h -d 1'
+alias pubip='curl icanhazip.com'
+alias myip="ifconfig | grep inet | grep -vE '(inet6|127.0.0.1)' | awk '{print $2}' | awk -F ':' '{print $2}'"
+#alias myip='ifconfig en0 inet | grep inet'
 
 # Applications
 alias vsc='open -a "Visual Studio Code"'
