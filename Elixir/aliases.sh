825d7860087def1cceaab78e2c39c18f2fafeff7
Sheharyar Naseer <hello@sheharyar.me>
2016-11-03 21:16:53 +0500

    New alias `epsc` for starting console+server together

--- Elixir/aliases.sh
+++ Elixir/aliases.sh
@@ -1,6 +1,7 @@
 # Elixir Aliases
 # --------------
 
-alias eps='mix phoenix.server'
-alias epc='iex -S mix phoenix.server'
+alias  eps='mix phoenix.server'
+alias  epc='iex -S mix'
+alias epsc='iex -S mix phoenix.server'
 
