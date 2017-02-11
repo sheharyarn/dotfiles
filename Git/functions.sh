52b405c5f567b5cf795b6fc7bc048a7ba191d73a
Sheharyar Naseer <hello@sheharyar.me>
2017-02-11 14:04:52 -0500

    Updated gc-time to use gdate on OSX

--- Git/functions.sh
+++ Git/functions.sh
@@ -19,21 +19,28 @@ gc() {
 # Commits in a specific instance in time
 # See: http://stackoverflow.com/a/3896112/1533054
 gc-time() {
-    if [[ $# -ne 2 ]] ; then
-        echo "invalid arguments"
-        echo "usage: gc-time DATETIME COMMIT_MESSAGE"
-        echo "examples:"
-        echo "   gc-time \"5 days ago\" \"Commit 5 days in the past\""
-        echo "   gc-time \"4 hours\" \"Commit 4 hours in the future\""
-        echo "   gc-time \"Fri Jul 26 19:32:10 2013 -0400\" \"Commit in specific instance in time\""
-        echo ""
-    else
-        if (date -d "$1") &>/dev/null; then
-            local gitdate=$(date -d "$1")
-            GIT_AUTHOR_DATE="$gitdate" GIT_COMMITTER_DATE="$gitdate" git commit -m "$2"
-        else
-            gc-time
-        fi
-    fi
+if [[ $# -ne 2 ]] ; then
+  echo "invalid arguments"
+  echo "usage: gc-time DATETIME COMMIT_MESSAGE"
+  echo "examples:"
+  echo "   gc-time \"5 days ago\" \"Commit 5 days in the past\""
+  echo "   gc-time \"4 hours\" \"Commit 4 hours in the future\""
+  echo "   gc-time \"Fri Jul 26 19:32:10 2013 -0400\" \"Commit in specific instance in time\""
+  echo ""
+else
+
+  if [ "$(get-platform)" == 'linux' ]; then
+    local date="date"
+  elif [ "$(get-platform)" == 'apple' ]; then
+    local date="gdate"
+  fi
+
+  if ($date -d "$1") &>/dev/null; then
+    local gitdate=$($date -d "$1")
+    GIT_AUTHOR_DATE="$gitdate" GIT_COMMITTER_DATE="$gitdate" git commit -m "$2"
+  else
+    gc-time
+  fi
+fi
 }
 
