ad4c966d726cc4e466921a12c91cfc2c8446c3fb
Sheharyar Naseer <hello@sheharyar.me>
2017-05-31 17:59:20 -0400

    Removed the wordfrequency function

--- Zsh/functions.sh
+++ Zsh/functions.sh
@@ -159,21 +159,6 @@ extract () {
   fi
 }
 
-# Get Word Freqeuncy of Files
-function wordfrequency() {
-  awk '
-     BEGIN { FS="[^a-zA-Z]+" } {
-         for (i=1; i<=NF; i++) {
-             word = tolower($i)
-             words[word]++
-         }
-     }
-     END {
-         for (w in words)
-              printf("%3d %s\n", words[w], w)
-     } ' | sort -rn
-}
-
 
 # Disposable File Hosting - http://transfer.sh/
 transfer() {
