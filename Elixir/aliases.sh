2b45832c62180a33c10658cba1d000e9d1db1701
Sheharyar Naseer <hello@sheharyar.me>
2017-08-27 15:37:42 -0400

    Ecto reset aliases

--- Elixir/aliases.sh
+++ Elixir/aliases.sh
@@ -6,3 +6,6 @@ alias  exs='mix phoenix.server'
 alias  exc='iex -S mix'
 alias exsc='iex -S mix phoenix.server'
 
+alias ex.reset='mix ecto.reset && MIX_ENV=test mix ecto.reset'
+alias ex.reset.test='ex.reset && mix test'
+
