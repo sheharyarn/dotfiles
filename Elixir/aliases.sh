c97c01a750852ee7d25921bf648264d19b81fe36
Sheharyar Naseer <hello@sheharyar.me>
2017-12-24 20:12:23 -0500

    Persistent Elrang/Elixir shell history (OTP 20+)

--- Elixir/aliases.sh
+++ Elixir/aliases.sh
@@ -9,3 +9,11 @@ alias exsc='iex -S mix phoenix.server'
 alias ex.reset='mix ecto.reset && MIX_ENV=test mix ecto.reset'
 alias ex.reset.test='ex.reset && mix test'
 
+
+
+# Other Settings
+# --------------
+
+# Enable Persistent Shell History (OTP 20+)
+export ERL_AFLAGS="-kernel shell_history enabled"
+
