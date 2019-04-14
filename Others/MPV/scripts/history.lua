6eedf1df939d882902c00bb44f1a36de09308696
Sheharyar Naseer <hello@sheharyar.me>
2019-04-14 13:58:03 -0700

    Keep track of all files you play on mpv

---
+++ Others/MPV/scripts/history.lua
@@ -0,0 +1,13 @@
+local HISTFILE = os.getenv('HOME')..'/.mpv_history';
+
+mp.register_event('file-loaded', function()
+  local title, fp;
+
+  title = mp.get_property('media-title');  
+  title = (title == mp.get_property('filename') and '' or (' (%s)'):format(title));
+  -- title = ''; -- uncomment here
+
+  fp = io.open(HISTFILE, 'a+');
+  fp:write(('[%s] %s%s\n'):format(os.date('%Y-%m-%d %X'), mp.get_property('path'), title));    
+  fp:close();
+end)
