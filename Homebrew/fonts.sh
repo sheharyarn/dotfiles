55b14e3db961e82c0c719c35297b85858b9de619
Sheharyar Naseer <hello@sheharyar.me>
2019-06-16 11:30:22 -0700

    Homebrew file for favorite fonts

---
+++ Homebrew/fonts.sh
@@ -0,0 +1,14 @@
+brew cask install font-roboto
+brew cask install font-noto-sans
+brew cask install font-fira-code
+
+# Font Awesome 4
+brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask-fonts/7a6038460ea23b2eb21e0cabf1adfab33553fcdc/Casks/font-fontawesome.rb
+
+# Font Awesome 5
+brew cask install font-fontawesome
+
+# NERD Fonts
+brew cask install font-hack-nerd-font
+brew cask install font-fura-nerd-font
+
