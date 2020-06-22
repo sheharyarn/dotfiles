d1a7cdb05f532b7459f6115a8df1ec967bd58c4b
Sheharyar Naseer <hello@sheharyar.me>
2020-06-22 05:14:03 +0500

    v-in function to open vim in a specific dir

--- Vim/functions.sh
+++ Vim/functions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
 # Vim Functions
 # ============
 
+
+# Open Vim
 v() {
   if hash nvim 2>/dev/null; then
     nvim "$@"
@@ -12,5 +15,16 @@ v() {
     gvim "$@"
   else
     vim "$@"
+  fi
+}
+
+
+# Open vim in a specific directory
+v-in() {
+  if [[ $# -eq 1 ]] ; then
+    cd "$1" && v && popd 1>/dev/null
+  else
+    echo "usage: v-in [DIRECTORY]"
+    return 1
   fi
 }
