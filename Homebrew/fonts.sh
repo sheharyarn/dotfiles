eebb96a40b1edd5391a0ee3e702e45e6425e3c95
Sheharyar Naseer <hello@sheharyar.me>
2022-01-02 22:55:29 -0800

    Clean up brew fonts script

--- Homebrew/fonts.sh
+++ Homebrew/fonts.sh
@@ -1,14 +1,16 @@
-brew cask install font-roboto
-brew cask install font-noto-sans
-brew cask install font-fira-code
+brew install --cask font-roboto
+brew install --cask font-noto-sans
+brew install --cask font-fira-code
+brew install --cask font-jetbrains-mono
 
 # Font Awesome 4
-brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask-fonts/7a6038460ea23b2eb21e0cabf1adfab33553fcdc/Casks/font-fontawesome.rb
+brew install --cask https://raw.githubusercontent.com/Homebrew/homebrew-cask-fonts/7a6038460ea23b2eb21e0cabf1adfab33553fcdc/Casks/font-fontawesome.rb
 
 # Font Awesome 5
-brew cask install font-fontawesome
+brew install --cask font-fontawesome
 
 # NERD Fonts
-brew cask install font-hack-nerd-font
-brew cask install font-fura-nerd-font
+brew install --cask font-hack-nerd-font
+brew install --cask font-fura-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
 
