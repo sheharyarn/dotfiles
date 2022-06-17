fbcddb0342c1eae46e8116b187afbc5db321b3fd
Sheharyar Naseer <hello@sheharyar.me>
2022-06-17 16:40:32 -0700

    Zsh: dotenv local

--- Zsh/functions.sh
+++ Zsh/functions.sh
@@ -133,6 +133,29 @@ function $ {
 }
 
 
+# dotenv local implmentation
+dotenv() {
+  bash -c "
+  path=\$(pwd)
+  while [[ \"\$path\" != \"\" && ! -e \"\$path/.env\" ]]; do
+    path=\${path%/*}
+  done
+
+
+  if [[ -e \"\$path/.env\" ]]; then
+    set -a
+    source \"\$path/.env\"
+    set +a
+    exec $*
+  else
+    echo \"Error: .env not found!\"
+    exit 1
+  fi
+  "
+}
+
+
+
 # Extract all archives with a single command
 extract () {
   if [ $# -eq 1 ] ; then
