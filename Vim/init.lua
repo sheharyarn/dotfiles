529eba9c96067c2ab6e793f2ae1a20f498999359
Sheharyar Naseer <hello@sheharyar.me>
2022-06-17 16:41:31 -0700

    Update Vim Dashboard

--- Vim/init.lua
+++ Vim/init.lua
@@ -236,11 +236,12 @@ local dashboard = require('alpha.themes.dashboard')
 dashboard.section.header.val = intro_header
 
 dashboard.section.buttons.val = {
-  dashboard.button('e', '   New file' , ':ene <BAR> startinsert <CR>'),
-  dashboard.button('f', '   Find file', ':cd $HOME/code | Telescope find_files<CR>'),
-  dashboard.button('r', '   Recent'   , ':Telescope oldfiles<CR>'),
-  dashboard.button('s', '   Settings' , ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'),
-  dashboard.button('q', '   Quit'     , ':qa<CR>'),
+  dashboard.button('i',  '   New file' , ':ene <BAR> startinsert <CR>'),
+  dashboard.button('ff', '   Find file', ':Telescope find_files<CR>'),
+  dashboard.button('fg', '   Find text', ':Telescope live_grep<CR>'),
+  dashboard.button('fr', '   Recent'   , ':Telescope oldfiles<CR>'),
+  dashboard.button('s',  '   Settings' , ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'),
+  dashboard.button('q',  '   Quit'     , ':qa<CR>'),
 }
 
 alpha.setup(dashboard.config)
