9cedc7767cf5ddd46d477566a215143df61c6724
Sheharyar Naseer <hello@sheharyar.me>
2022-08-26 12:53:59 -0700

    Brew: Add Roboto fonts

--- Homebrew/fonts.sh
+++ Homebrew/fonts.sh
@@ -1,16 +1,23 @@
+# Roboto Family
 brew install --cask font-roboto
+brew install --cask font-roboto-flex
+brew install --cask font-roboto-serif
+brew install --cask font-roboto-slab
+brew install --cask font-roboto-mono
+brew install --cask font-roboto-mono-nerd-font
+
+# JetBrains Mono
+brew install --cask font-jetbrains-mono
+brew install --cask font-jetbrains-mono-nerd-font
+
+# Other Dev/NERD Fonts
 brew install --cask font-noto-sans
 brew install --cask font-fira-code
-brew install --cask font-jetbrains-mono
+brew install --cask font-hack-nerd-font
+brew install --cask font-fura-nerd-font
 
 # Font Awesome 4
 brew install --cask https://raw.githubusercontent.com/Homebrew/homebrew-cask-fonts/7a6038460ea23b2eb21e0cabf1adfab33553fcdc/Casks/font-fontawesome.rb
 
 # Font Awesome 5
 brew install --cask font-fontawesome
-
-# NERD Fonts
-brew install --cask font-hack-nerd-font
-brew install --cask font-fura-nerd-font
-brew install --cask font-jetbrains-mono-nerd-font
-
