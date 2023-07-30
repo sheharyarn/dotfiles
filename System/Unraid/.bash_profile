c318b8e0b4b244fc82bb90c8948d3d2e1a4bc35c
Sheharyar Naseer <hello@sheharyar.me>
2023-07-29 18:22:36 -0700

    Unraid: Update boot and bash_profile

--- System/Unraid/.bash_profile
+++ System/Unraid/.bash_profile
@@ -10,6 +10,10 @@ xterm*|rxvt*)
     ;;
 esac
 
+# Set Dotfiles Location
+# (Note this is not the original location, this is just a symlink)
+export DOTFILES='/root/.dotfiles'
+
 # impersonate a user
 alias user="su -ls /bin/bash"
 
@@ -17,3 +21,8 @@ alias  l='ls -FG'
 alias la='ls -FGalh'
 alias ll='ls -FG1'
 
+alias  v='vim'
+alias vi='vim'
+
+source "$DOTFILES/Git/aliases.sh"
+#source "$DOTFILES/Git/functions.sh"
--- System/Unraid/boot.sh
+++ System/Unraid/boot.sh
@@ -18,3 +18,17 @@ if [ -d "$DOTFILES_PATH" ]; then
   ln -s $DOTFILES_PATH /root/.dotfiles
   ln -s /root/.dotfiles/System/Unraid/.bash_profile /root/.bash_profile
 fi
+
+# Create Share Links
+if [ ! -d "/unraid" ]; then
+  mkdir -p /unraid
+
+  ln -s /unraid      /root/unraid
+  ln -s /mnt/shares  /unraid/shares
+  ln -s /mnt/disks   /unraid/disks
+  ln -s /mnt/remotes /unraid/remotes
+fi
+
+# Set Git Config
+git config --global user.name "Sheharyar Naseer"
+git config --global user.email "hello@sheharyar.me"
