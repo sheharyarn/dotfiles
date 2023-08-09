63021a811f169c4f65519359a37b2a006414d2ff
Sheharyar Naseer <hello@sheharyar.me>
2023-08-09 01:34:40 -0700

    Vim: Move common utils into new file

--- Vim/lua/utils.lua
+++ Vim/lua/utils.lua
@@ -32,3 +32,75 @@ function table_contains(table, element)
 
   return false
 end
+
+
+function sleep(seconds)
+  local ntime = os.time() + seconds
+  repeat until os.time() > ntime
+end
+
+
+-- Check if module exists
+function is_module_available(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == 'function' then
+        package.preload[name] = loader
+        return true
+      end
+    end
+    return false
+  end
+end
+
+
+
+-- Generate Plugin Snapshot
+create_snapshot = function()
+  local SNAPSHOT_PATH = VIM_DIR .. '/plugins.snapshot';
+
+  -- Ensure Packer is available
+  if is_module_available('packer') then
+    local packer = require('packer')
+    local snapshot = require('packer.snapshot')
+    local async = require('packer.async').sync
+    local await = require('packer.async').wait
+    local tempfile = vim.fn.tempname()
+    local sorted =  VIM_DIR .. '/plugins.sorted'
+
+    -- Delete Snapshot if an older one already exists
+    if vim.fn.filereadable(SNAPSHOT_PATH) then
+      vim.fn.delete(SNAPSHOT_PATH)
+    end
+
+    packer.snapshot(SNAPSHOT_PATH)
+
+    --async(function()
+    --  --packer.__manage_all()
+    --  --local target_plugins = plugins
+    --  --
+    --  --await(snapshot.create(SNAPSHOT_PATH, {}))
+    --  --
+
+    --  --vim.fn.delete(tempfile)
+    --  sleep(2)
+    --  os.execute('jq --sort-keys . ' .. SNAPSHOT_PATH .. ' > ' .. sorted)
+    --end)()
+    --print(tempfile)
+  else
+    print('Packer Unavailable')
+  end
+end
+
+-- Split string into an array
+split_string = function(source, delimiters)
+  local elements = {}
+  local pattern = '([^'..delimiters..']+)'
+  string.gsub(source, pattern, function(value)
+    elements[#elements + 1] = value
+  end)
+  return elements
+end
