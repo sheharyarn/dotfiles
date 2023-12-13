bcb97390543f03b553fcbd86438c7c0855dad50a
Sheharyar Naseer <hello@sheharyar.me>
2023-12-12 20:43:40 -0700

    MPV: Cleanup history script

--- MPV/scripts/history.lua
+++ MPV/scripts/history.lua
@@ -3,11 +3,11 @@ local HISTFILE = os.getenv('HOME')..'/.mpv_history';
 mp.register_event('file-loaded', function()
   local title, fp;
 
-  title = mp.get_property('media-title');  
+  title = mp.get_property('media-title');
   title = (title == mp.get_property('filename') and '' or (' (%s)'):format(title));
   -- title = ''; -- uncomment here
 
   fp = io.open(HISTFILE, 'a+');
-  fp:write(('[%s] %s%s\n'):format(os.date('%Y-%m-%d %X'), mp.get_property('path'), title));    
+  fp:write(('[%s] %s%s\n'):format(os.date('%Y-%m-%d %X'), mp.get_property('path'), title));
   fp:close();
 end)
