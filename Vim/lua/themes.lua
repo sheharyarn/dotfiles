1f34ace189b174c2a0f6b3f93449e54980af44e1
Sheharyar Naseer <hello@sheharyar.me>
2023-12-22 20:46:42 -0700

    Vim: Add basic docs to themes.lua

--- Vim/lua/themes.lua
+++ Vim/lua/themes.lua
@@ -1,24 +1,36 @@
 
--------------------------
--- Custom Color Scheme --
--------------------------
+----------------------------
+-- Custom Styles & Themes --
+----------------------------
 
+
+-- Define or load Special Colors
+local MColors = require('material.colors').main
 local Colors = {
   black = '#090b10',
   red = '#df7d85',
 }
 
-local MColors = require('material.colors').main
+-- Enable 24-bit RGB color if available
+vim.o.termguicolors = true
 
---highlight = function(name, opts)
---  local args = 'highlight ' .. name
---
---  if opts.fg then args = args .. ' guifg=' .. opts.fg end
---  if opts.bg then args = args .. ' guibg=' .. opts.bg end
---
---  vim.cmd(args)
---end
 
+
+-- Apply highlight colors in NeoVim
+-- ---------------------------------
+-- Takes group name and a table of options which can include any
+-- of the following. Any options not set are defaulted to
+-- blank/none/empty:
+--  * fg
+--  * bg
+--  * sp
+--  * link
+--
+--  Example:
+--  highlight('LineNr', { fg='red' bg='black' })
+--
+-- @param group Highlight Group Name
+-- @param opts Table of options/arguments
 highlight = function(group, opts)
   local style = opts.style and "gui=" .. opts.style or "gui=NONE"
   local fg = opts.fg and "guifg=" .. opts.fg or "guifg=NONE"
@@ -32,6 +44,13 @@ highlight = function(group, opts)
   end
 end
 
+
+
+-- Highlight dangling whitespace after content
+-- -------------------------------------------
+-- Special highlight scheme that should be applied as part of
+-- all custom themes. This highlights whitespace characters at
+-- the end of lines with dark red color.
 highlightWhitespace = function(enable)
   enable = enable or true
   opts = enable and { bg = MColors.darkred } or {}
@@ -40,9 +59,6 @@ highlightWhitespace = function(enable)
   vim.cmd([[autocmd Syntax * syn match ExtraWhitespace /\s\+$\| \+\ze\t/]])
   highlight('ExtraWhitespace', opts)
 end
-
--- Enable 24-bit RGB color if available
-vim.o.termguicolors = true
 
 local run_async = function(fun)
   local async
@@ -53,7 +69,15 @@ local run_async = function(fun)
   async:send()
 end
 
+
+
+-- Define Custom Themes
+-- --------------------
+-- This sets a Global 'PsyThemes' object in NeoVim
 PsyThemes = {
+  -- Set default dark and light themes
+  dark  = function() PsyThemes.material_dark()   end,
+
   material_dark = function()
     require('material').setup({
       plugins = {
@@ -86,7 +110,7 @@ PsyThemes = {
       highlight('Search', { bg = MColors.darkyellow, fg = Colors.black })
       highlightWhitespace()
     end)
-  end
+  end,
 }
 
 
