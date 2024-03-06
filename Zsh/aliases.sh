bbae67efccbd498009ed812dad035482a7d0c4de
Sheharyar Naseer <hello@sheharyar.me>
2024-03-05 17:04:10 -0700

    Zsh: Replace cd with z for common aliases

--- Zsh/aliases.sh
+++ Zsh/aliases.sh
@@ -9,10 +9,11 @@ alias la='ls -FGalh'
 alias ll='ls -FG1'
 
 # Directories
-alias dotfiles="cd $dotfiles"
-alias     crap="cd ~/crap"
-alias     code="cd ~/code"
-alias  clients="cd ~/code/sruplex/clients"
+alias      _cd="z"
+alias dotfiles="_cd $dotfiles"
+alias     crap="_cd ~/crap"
+alias     code="_cd ~/code"
+alias  clients="_cd ~/code/sruplex/clients"
 
 # Edit .zshrc file
 alias zshrc="$EDITOR $zshrc"
